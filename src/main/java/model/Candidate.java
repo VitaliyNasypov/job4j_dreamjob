0e20723df23fd92a8b7d9b6968b5ab89d60c7776
Vitaliy Nasypov <nasvit25@gmail.com>
2020-12-12 19:32:55 +0700

    Проект "Работа мечты" #408587

---
+++ src/main/java/model/Candidate.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Candidate {
+    private String lastName;
+    private String firstName;
+    private int age;
+
+    public void AddResume() {
+
+    }
+}
---
+++ src/main/java/model/HumanResources.java
@@ -0,0 +1,10 @@
+package model;
+
+public class HumanResources {
+    private String lastName;
+    private String firstName;
+
+    public void AddJob() {
+
+    }
+}
