90322d518642a82139d3f7b5429afb1b0a1e385d
Vitaliy Nasypov <nasvit25@gmail.com>
2020-12-13 09:20:44 +0700

    4. candicates.jsp - список кандидатов. #408595

---
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -0,0 +1,53 @@
+package ru.job4j.dream.model;
+
+public class Candidate {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Candidate(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Candidate candidate = (Candidate) o;
+
+        if (id != candidate.id) return false;
+        if (age != candidate.age) return false;
+        if (firstName != null ? !firstName.equals(candidate.firstName) : candidate.firstName != null) return false;
+        return lastName != null ? lastName.equals(candidate.lastName) : candidate.lastName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
+}
