78f19382e3bd800cecdd16730c0f2b63c5fb9f7b
Vitaliy Nasypov <nasvit25@gmail.com>
2021-01-09 18:36:44 +0700

    4.1. JSON формат. #408580

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,9 +1,15 @@
 package ru.job4j.dream.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 public class GreetingServlet extends HttpServlet {
@@ -17,5 +23,32 @@ public class GreetingServlet extends HttpServlet {
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         writer.println("Nice to meet you, " + name);
         writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String jsonInput = br.readLine();
+        Gson gson = new Gson();
+        ObjectJson objectJson = gson.fromJson(jsonInput, ObjectJson.class);
+        objectJson.setText("Nice to meet you, " + objectJson.getText());
+        String jsonOutput = gson.toJson(objectJson);
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().print(jsonOutput);
+        resp.getWriter().flush();
+    }
+
+    private static class ObjectJson {
+        @SerializedName("text")
+        private String text;
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
     }
 }
